6594943a1243ceab5bb5454d085ca72e0d391dea
David Czeller <david.czeller@cloudcode.hu>
2021-01-22 00:41:16 +0100

    linnechart fix

--- src/Components/Charts/LineChart.jsx
+++ src/Components/Charts/LineChart.jsx
@@ -16,18 +16,6 @@ export default function LineChart(props) {
     value,
   } = props;
 
-  // console.log(title, value)
-
-  const keys = value && Object.keys(value)
-  const values = value && Object.values(value)
-
-  // const orders = value && value.length && value.map(orders => orders)
-  // const order = orders && orders.map(order => order[1].length.toString())
-  // console.log(orders && orders[0][0], orders && orders[0][1].length)
-
-  // const a = values.map(item => item)
-  // console.log(keys, values, values && values.map(item => item))
-
   return (
     <>
       <div className='chart_title' >
@@ -36,11 +24,13 @@ export default function LineChart(props) {
       {value && (
         <VictoryChart>
           <VictoryLine
-            data={values && values.map(item => item.length)}
-            // data accessor for x values
-            // x={keys && keys.map(item => item)}
-            // data accessor for y values
-            // y={values && values.map(item => item.length)}
+          style={{
+            data: { stroke: "#445FFF" }
+          }}
+            data={value.map(supplierData => ({
+              x: supplierData.date,
+              y: supplierData.volume
+            }))}
           />
         </VictoryChart>
       )}
