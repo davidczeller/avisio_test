c1e4bc5282076a959aa5f38fdbfdcd9f73fb58bd
David Czeller <david.czeller@cloudcode.hu>
2021-01-22 00:42:33 +0100

    get all data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 import Paper from './Components/Common/Paper'
 import LineChart from './Components/Charts/LineChart'
+import BarChart from './Components/Charts/BarChart'
 import Tooltip from './Components/Common/Tooltip'
 import './App.scss';
 
@@ -27,19 +28,50 @@ function App() {
     setData()
   }, [])
 
-  const orderByDay = () => data && data.reduce((acc, order) => {
-    if (!acc[order.orderedOn]) {
-      acc[order.orderedOn] = []
-    }
-    acc[order.orderedOn].push(order)
+  const getVolume = (orders) => (
+    orders.reduce((acc, order) => {
+      acc += (order.price * parseInt(order.quantity, 10))
+      return acc
+    }, 0)
+  )
 
-    Object.keys(acc)
+  // const selectedSupplier = 'Yum Food'
+  // const selectedProductCategory1 = 'food'
+  // const selectedProductCategory2 = 'bread'
 
-    Object.values(acc)
+  const selectedSupplier = 'all'
+  const selectedProductCategory1 = 'all'
+  const selectedProductCategory2 = 'all'
 
-    setOrdersByDay(Object.entries(acc))
-    return acc
-  }, {})
+
+  const orderByDayData = () => data && data
+    .filter(o => o.supplier === selectedSupplier || selectedSupplier === 'all')
+    .filter(o => o.productCategory1 === selectedProductCategory1 || selectedProductCategory1 === 'all')
+    .filter(o => o.productCategory2 === selectedProductCategory2 || selectedProductCategory2 === 'all')
+    .reduce((acc, order) => {
+      if (!acc[order.orderedOn]) {
+        acc[order.orderedOn] = []
+      }
+      acc[order.orderedOn].push(order)
+
+      setOrdersByDay(Object.entries(acc))
+      return acc
+    }, {})
+  // .map(x => (x
+  // {
+  //   date: x[0],
+  //   volume: getVolume(x[1])
+  // }
+  // ))
+
+  //every day volume
+  const orderByDay = ordersByDay && ordersByDay.map(x => ({
+    date: x[0],
+    volume: getVolume(x[1])
+  }))
+
+  console.log(orderByDay && orderByDay)
+
 
   const orderByProduct = () => data && data.reduce((acc, order) => {
     if (!acc[order.productName]) {
@@ -90,10 +122,6 @@ function App() {
     }
     acc[order.supplier].push(order)
 
-    Object.keys(acc)
-
-    Object.values(acc)
-
     setOrdersBySuppliers(Object.entries(acc))
     return acc
   }, {})
@@ -112,10 +140,19 @@ function App() {
     return acc
   }, {})
 
+
   const deliveryDates = ordersByDeliveryDate && ordersByDeliveryDate.map(item => ({
     date: item[0],
-    suppliers: item[1].map(item => item.supplier)
+    suppliers: item[1].map(item => item.supplier).reduce((acc, supplier) => {
+      if (!acc[supplier]) {
+        acc[supplier] = 0
+      }
+      acc[supplier]++
+
+      return acc
+    }, {})
   }))
+
 
   const [asd, setAsd] = useState()
 
@@ -133,11 +170,23 @@ function App() {
     return acc
   }, {})
 
-  console.log({ ordersBySuppliers, ordersByDeliveryDate, deliveryDates })
+  const supplierData = ordersBySuppliers && ordersBySuppliers.map(supplier => ({
+    supplier: supplier[0],
+    volume: getVolume(supplier[1]),
+    quantity: supplier[1].reduce((acc, order) => {
+      acc += parseInt(order.quantity, 10)
+      return acc
+    }, 0),
+  }))
+
+  //sort
+
+
+  console.log({ ordersByDay, ordersBySuppliers, ordersByDeliveryDate, deliveryDates, supplierData })
 
   useEffect(() => {
     products()
-    orderByDay()
+    orderByDayData()
     orderByProduct()
     orderBySuppliers()
     orderByDeliveryDate()
@@ -173,19 +222,19 @@ function App() {
                 <div className="row_title">
                   <div className="title">Products</div>
                   {ordersByProduct && ordersByProduct.slice(0, 3).map(item =>
-                    <p>{item.product}</p>
+                    <p key={item.cost}>{item.product}</p>
                   )}
                 </div>
                 <div className="row_title">
                   <div className="title">Quantity</div>
                   {ordersByProduct && ordersByProduct.slice(0, 3).map(item =>
-                    <p>{item.quantity}</p>
+                    <p key={item.cost}>{item.quantity}</p>
                   )}
                 </div>
                 <div className="row_title">
                   <div className="title">Cost</div>
                   {ordersByProduct && ordersByProduct.slice(0, 3).map(item =>
-                    <p>€ {item.cost}</p>
+                    <p key={item.cost}>€ {item.cost}</p>
                   )}
                 </div>
               </div>
@@ -194,13 +243,33 @@ function App() {
           }
         />
         <Paper
+          headerText='Deliveries'
+          content={
+            deliveryDates && deliveryDates.map(delivery => (
+              <>
+                <div>{delivery.date}</div>
+                {Object.entries(delivery.suppliers).map(kvp => (
+                  <div key={`${delivery.date}_${kvp[0]}`}>{kvp[0]} {kvp[1]}</div>
+                ))}
+              </>
+            ))
+          }
+        />
+        <Paper
           headerText='Order Volume'
           content={
             <LineChart
               title=''
-              value={
-                asd
-              }
+              value={orderByDay}
+            />
+          }
+        />
+        <Paper
+          headerText='Supplier Ranking'
+          content={
+            <BarChart
+              title=''
+              value={supplierData}
             />
           }
         />
