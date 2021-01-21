a6b57718c7b267b9f3fd7fe21f8920c84a704796
David Czeller <david.czeller@cloudcode.hu>
2021-01-21 22:17:10 +0100

    Tooltip added

---
+++ src/Components/Common/Tooltip.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react"
+import "./Tooltip.scss"
+
+export default function Tooltip({ delay, direction, children, content }) {
+  let timeout
+  const [active, setActive] = useState(false)
+
+  const showTip = () => {
+    timeout = setTimeout(() => {
+      setActive(true)
+    }, delay || 400)
+  }
+
+  const hideTip = () => {
+    clearInterval(timeout)
+    setActive(false)
+  }
+
+  return (
+    <div
+      className="Tooltip-Wrapper"
+      // When to show the tooltip
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
+    >
+      {/* Wrapping */}
+      {children}
+      {active && (
+        <div className={`Tooltip-Tip ${direction || "top"}`}>
+          {/* Content */}
+          {content}
+        </div>
+      )}
+    </div>
+  )
+}
