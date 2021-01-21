52aa7676dfeba1902b28be59e9499ed0c6f51f9f
David Czeller <david.czeller@cloudcode.hu>
2021-01-21 22:19:25 +0100

    some changes in Paper component, buttonn added, scss fixes

--- src/Components/Common/Paper.jsx
+++ src/Components/Common/Paper.jsx
@@ -1,16 +1,46 @@
 import React from 'react'
 
 import './Paper.scss'
+import Tooltip from '../Common/Tooltip'
 
 import { useStateProviderValue } from '../../Services/StateProvider'
 
-export default function Paper({headerText, content}) {
-  const [{data},] = useStateProviderValue();
+export default function Paper({ headerText, content, isButton }) {
+  const [{ data, sortType }, dispatch] = useStateProviderValue();
 
-  console.log(content)
-   return (
+  const setSortType = () => {
+    if (sortType === 'cost') {
+      dispatch({
+        type: 'SET_SORT_TYPE',
+        sortType: 'quantity'
+      })
+    } else {
+      dispatch({
+        type: 'SET_SORT_TYPE',
+        sortType: 'cost'
+      })
+    }
+  }
+
+  return (
     <div className='paper_container'>
-      <div className="header">{headerText}</div>
+      <div className="top_container">
+        <div className="header">{headerText}</div>
+        {isButton && (
+          <Tooltip
+            content={sortType === 'cost' ? 'Sort By Quantity' : 'Sort By Cost'}
+            direction="right">
+            <button
+              className='sort_button'
+              onClick={() => setSortType()}>
+              {sortType === 'cost'
+                ? <img src="https://img.icons8.com/ios/25/000000/euro-pound-exchange.png" />
+                : <img src="https://img.icons8.com/ios/23/000000/negative-dynamic.png" />
+              }
+            </button>
+          </Tooltip>
+        )}
+      </div>
       {content}
     </div>
   )
