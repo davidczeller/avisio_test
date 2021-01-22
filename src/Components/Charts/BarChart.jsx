8b389057c53629d47689893adfd98f1c1a9469b3
David Czeller <david.czeller@cloudcode.hu>
2021-01-22 20:41:53 +0100

    BarChart ui fixes

--- src/Components/Charts/BarChart.jsx
+++ src/Components/Charts/BarChart.jsx
@@ -17,19 +17,17 @@ export default function LineChart(props) {
   } = props;
 
   const [width, setWidth] = useState(window.innerWidth)
+  
   const updateWidth = (ev) => {
     setWidth(ev.target.innerWidth)
   }
 
-  // useEffect replaces `componentDidMount` and others
   useEffect(() => {
     window.addEventListener('resize', updateWidth)
 
-    // Removes listener on unmount
     return () => {
       window.removeEventListener('resize', updateWidth)
     }
-
   }, [])
 
   return (
@@ -40,25 +38,22 @@ export default function LineChart(props) {
       {value && (
         <svg viewBox={"0 0" + " " + width + " " + "350"} preserveAspectRatio="none" width="100%" height="100%">
           <VictoryChart
-            domainPadding={{ y: 40 }}
+            domainPadding={{ y: 20, x: 20 }}
             standalone={false}
             width={width}
-          // height={450}
           >
-            <VictoryAxis tickFormat={() => ''} />
             <VictoryBar
               horizontal
               barWidth={({ index }) => index * 2 + 8}
               style={{
-                data: { fill: "#445FFF" }
+                data: {
+                  fill: "#2F4EFE",
+                  stroke: "#2F4EFE", strokeWidth: 8,
+                },
+                labels: {
+                  fontSize: 15,
+                }
               }}
-              labelComponent={
-                <VictoryLabel
-                  dy={-20}
-                  dx={0}
-                  textAnchor="start"
-                />
-              }
               data={value.map(supplierData => ({
                 label: supplierData.supplier,
                 x: supplierData.supplier,
